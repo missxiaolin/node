4eb9603d69dd1747ad60136e8ecd72868622c425
xiaolin <462441355@qq.com>
2018-10-03 13:19:35 +0800

    * es6 编译

--- es6/src/index.js
+++ es6/src/index.js
@@ -1,1 +1,11 @@
-alert(1)
+class Person {
+    constructor(name){
+        this.name = name
+    }
+    
+    getNanem(){
+        return this.name
+    }
+}
+let p = new Person('xiaolin')
+alert(p.getNanem())
--- es6/webpack.dev.config.js
+++ es6/webpack.dev.config.js
@@ -1,16 +1,26 @@
+const webpack = require('webpack')
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 
 module.exports = {
-    entry: './src/index.js',
+    entry: "./src/index.js",
     output: {
         path: __dirname,
-        filename: './release/bundle.js'
+        filename: "./release/bundle.js"
     },
+
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/, 
+            loader: "babel-loader"
+        }]
+    },
+
     plugins: [
         new HtmlWebpackPlugin({
-            template: './index.html'
+            template: "./index.html"
         })
     ],
     devServer: {
