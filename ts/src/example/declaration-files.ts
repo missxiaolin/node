6f82bb7c82f7aafcf9f108fc2d69517ea38be076
xiaolin <462441355@qq.com>
2019-09-20 23:28:26 +0800

    * add

---
+++ ts/src/example/declaration-files.ts
@@ -0,0 +1,8 @@
+import '../modules/add-methods-to-string.js'
+import getInfoFunction from '../modules/c.js'
+setTitle('lison')
+// console.log(documentTitle)
+// console.log(getTitle())
+const name = 'lison'
+// console.log(name.getFirstLetter())
+// console.log(getInfoFunction())
