7dc2744cbf3b625dab2c5e0223da5578e4a78533
xiaolin <462441355@qq.com>
2020-08-21 23:19:00 +0800

    * css 处理

--- node/webpack/webpack1/build/webpack.base.js
+++ node/webpack/webpack1/build/webpack.base.js
@@ -3,6 +3,7 @@ const prod = require("./webpack.prod");
 const path = require("path");
 const merge = require("webpack-merge");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 
 module.exports = (env) => { // env 环境变量
@@ -15,7 +16,24 @@ module.exports = (env) => { // env 环境变量
             rules: [
                 {
                     test: /\.css$/,
-                    use: ['style-loader', 'css-loader']
+                    use: [ // 是不是开发环境 如果是就用style-loader
+                        isDev?"style-loader":MiniCssExtractPlugin.loader,
+                        {
+                            loader: "css-loader",
+                            options: {
+                                // 给loader传递参数
+                                // 如果css文件引入其他文件@import
+                                importLoaders: 2
+                            }
+                        },
+                        "postcss-loader",
+                        "sass-loader"
+                    ]
+                },
+                {
+                    // 匹配到scss结尾的使用sass-loader 来调用node-sass处理sass文件
+                    test: /\.scss$/,
+                    use: ["style-loader", "css-loader", "sass-loader"]
                 },
             ]
         },
@@ -24,6 +42,10 @@ module.exports = (env) => { // env 环境变量
             path: path.resolve(__dirname, "../dist")
         },
         plugins: [
+            // 在每次打包之前 先清除dist目录下的文件
+            !isDev && new MiniCssExtractPlugin({ // 如果是开发模式就不要使用抽离样式的插件
+                filename:'css/main.css'
+            }),
             new HtmlWebpackPlugin({
                 template: path.resolve(__dirname, "../public/index.html"),
                 filename: "index.html",
