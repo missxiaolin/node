c1fda198b98a714e7b20d679d0dee7b179d44831
xiaolin <462441355@qq.com>
2022-01-09 20:08:56 +0800

    * normalize css

--- blog1/src/App.js
+++ blog1/src/App.js
@@ -1,9 +1,10 @@
 import './style.scss'
 import styles from './style.module.scss'
+import 'normalize.css'
 
 function App() {
   return (
-    <div className={styles.app}>
+    <div className={styles.app }>
       hell word
     </div>
   );
