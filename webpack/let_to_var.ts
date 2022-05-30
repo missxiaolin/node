10aad57d8102df10c4695d5092d82d43ca163a02
xiaolin <462441355@qq.com>
2022-05-30 20:21:18 +0800

    * let 转换var

--- webpack/let_to_var.ts
+++ webpack/let_to_var.ts
@@ -7,4 +7,15 @@ const ast = parse(code, {
     sourceType: "module"
 })
 
-console.log(ast)
+traverse(ast, {
+    enter: item => {
+        if (item.node.type === 'VariableDeclaration') {
+            if(item.node.kind === 'let') {
+                item.node.kind = 'var'
+            }
+        }
+    }
+})
+const result = generate(ast, {}, code)
+
+console.log(result.code)
