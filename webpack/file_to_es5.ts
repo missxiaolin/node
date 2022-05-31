2cb1407cbc57a7109a8298fdac9608bc2d7c9cba
xiaolin <462441355@qq.com>
2022-05-31 21:43:42 +0800

    * 测试转换代码  es6->es5

---
+++ webpack/file_to_es5.ts
@@ -0,0 +1,17 @@
+// parse 把代码code 变成ast
+import { parse } from "@babel/parser";
+import * as babel from "@babel/core";
+import * as fs from 'fs'
+
+
+const code = fs.readFileSync('./test.js').toString()
+
+const ast = parse(code, { sourceType: "module" });
+
+const result = babel.transformFromAstSync(ast, code, {
+    presets: [
+        '@babel/preset-env'
+    ]
+})
+
+fs.writeFileSync('./test.es5.js', result.code)
---
+++ webpack/to_es5.ts
@@ -0,0 +1,14 @@
+// parse 把代码code 变成ast
+import { parse } from "@babel/parser";
+import * as babel from "@babel/core";
+
+const code = `let a = 'let';let b = 2; const c = 3`;
+const ast = parse(code, { sourceType: "module" });
+
+const result = babel.transformFromAstSync(ast, code, {
+    presets: [
+        '@babel/preset-env'
+    ]
+})
+
+console.log(result.code)
