ad6aee41bc896775fb24c3800cd9e004d93d29f4
xiaolin <462441355@qq.com>
2022-06-27 14:58:18 +0800

    * 格式化代码

--- webpack/bundler_1.ts
+++ webpack/bundler_1.ts
@@ -2,56 +2,59 @@
 // 请先运行 yarn 或 npm i 来安装依赖
 // 然后使用 node -r ts-node/register 文件路径 来运行，
 // 如果需要调试，可以加一个选项 --inspect-brk，再打开 Chrome 开发者工具，点击 Node 图标即可调试
-import { parse } from "@babel/parser"
-import traverse from "@babel/traverse"
-import { readFileSync } from 'fs'
-import { resolve, relative, dirname } from 'path';
-import * as babel from '@babel/core';
+import { parse } from "@babel/parser";
+import traverse from "@babel/traverse";
+import { readFileSync } from "fs";
+import { resolve, relative, dirname } from "path";
+import * as babel from "@babel/core";
 
 // 设置根目录
-const projectRoot = resolve(__dirname, 'project_1')
+const projectRoot = resolve(__dirname, "project_1");
 // 类型声明
-type DepRelation = { [key: string]: { deps: string[], code: string } }
+type DepRelation = { [key: string]: { deps: string[]; code: string } };
 // 初始化一个空的 depRelation，用于收集依赖
-const depRelation: DepRelation = {}
+const depRelation: DepRelation = {};
 
 // 将入口文件的绝对路径传入函数，如 D:\demo\fixture_1\index.js
-collectCodeAndDeps(resolve(projectRoot, 'index.js'))
+collectCodeAndDeps(resolve(projectRoot, "index.js"));
 
-console.log(depRelation)
-console.log('done')
+console.log(depRelation);
+console.log("done");
 
 function collectCodeAndDeps(filepath: string) {
-  const key = getProjectPath(filepath) // 文件的项目路径，如 index.js
+  const key = getProjectPath(filepath); // 文件的项目路径，如 index.js
   if (Object.keys(depRelation).includes(key)) {
-    console.warn(`duplicated dependency: ${key}`)
-    return
+    console.warn(`duplicated dependency: ${key}`);
+    return;
   }
   // 获取文件内容，将内容放至 depRelation
-  const code = readFileSync(filepath).toString()
+  const code = readFileSync(filepath).toString();
   const { code: es5Code } = babel.transform(code, {
-    presets: ['@babel/preset-env']
-  })
+    presets: ["@babel/preset-env"],
+  });
   // 初始化 depRelation[key]
-  depRelation[key] = { deps: [], code: es5Code }
+  depRelation[key] = { deps: [], code: es5Code };
   // 将代码转为 AST
-  const ast = parse(code, { sourceType: 'module' })
+  const ast = parse(code, { sourceType: "module" });
   // 分析文件依赖，将内容放至 depRelation
   traverse(ast, {
-    enter: path => {
-      if (path.node.type === 'ImportDeclaration') {
+    enter: (path) => {
+      if (path.node.type === "ImportDeclaration") {
         // path.node.source.value 往往是一个相对路径，如 ./a.js，需要先把它转为一个绝对路径
-        const depAbsolutePath = resolve(dirname(filepath), path.node.source.value)
+        const depAbsolutePath = resolve(
+          dirname(filepath),
+          path.node.source.value
+        );
         // 然后转为项目路径
-        const depProjectPath = getProjectPath(depAbsolutePath)
+        const depProjectPath = getProjectPath(depAbsolutePath);
         // 把依赖写进 depRelation
-        depRelation[key].deps.push(depProjectPath)
-        collectCodeAndDeps(depAbsolutePath)
+        depRelation[key].deps.push(depProjectPath);
+        collectCodeAndDeps(depAbsolutePath);
       }
-    }
-  })
+    },
+  });
 }
 // 获取文件相对于根目录的相对路径
 function getProjectPath(path: string) {
-  return relative(projectRoot, path).replace(/\\/g, '/')
+  return relative(projectRoot, path).replace(/\\/g, "/");
 }
