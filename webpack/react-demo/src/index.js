d91979c2a2a1ce40ca5c80314331ca057829b759
xiaolin <462441355@qq.com>
2022-08-20 21:28:55 +0800

    * 支持sass

--- webpack/react-demo/src/index.js
+++ webpack/react-demo/src/index.js
@@ -2,6 +2,7 @@ import a from '@/common/util'
 import './ts/index.ts'
 import { JsxDemo } from './jsx/index.js'
 import { tsxDemo } from './tsx/index.tsx'
+import './css/index.scss'
 
 console.log(1)
 console.log(tsxDemo)
--- webpack/react-demo/webpack.config.js
+++ webpack/react-demo/webpack.config.js
@@ -29,6 +29,17 @@ module.exports = {
             },
           },
         },
+        {
+          test: /\.s[ca]ss$/,
+          use: [
+            // 将 JS 字符串生成为 style 节点
+            'style-loader',
+            // 将 CSS 转化成 CommonJS 模块
+            'css-loader',
+            // 将 Sass 编译成 CSS
+            'sass-loader',
+          ]
+        }
       ],
   }
 };
