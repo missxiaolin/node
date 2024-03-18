84814e097b3272167c3fc27bd008511546147821
lingongbing <462441355@qq.com>
2024-03-18 16:59:12 +0800

    * add

--- dev/swiper/js/taobao.123.js
+++ dev/swiper/js/taobao.123.js
@@ -1,6 +1,30 @@
+// var swiper = new Swiper(".global-promotion", {
+//   slidesPerView: 1.12,
+//   slidesOffsetAfter: document.body.clientWidth * 0.115,
+//   watchSlidesProgress: true,
+//   on: {
+//     touchMove: function () {
+//       deg = this.progress * 4 * 45;
+//       this.$el
+//         .parents(".global")
+//         .find(".global-circle")
+//         .transform("rotate(-" + deg + "deg)");
+//     },
+//     setTransition: function (transition) {
+//       deg = this.progress * 4 * 45;
+//       this.$el
+//         .parents(".global")
+//         .find(".global-circle")
+//         .transition(transition)
+//         .transform("rotate(-" + deg + "deg) translate3d(0, 0, 0)");
+//     },
+//   },
+// });
+
 var swiper = new Swiper(".global-promotion", {
   slidesPerView: 1.12,
-  slidesOffsetAfter: document.body.clientWidth * 0.115,
+  slidesOffsetAfter: 15,
+  slidesOffsetBefore: 15,
   watchSlidesProgress: true,
   on: {
     touchMove: function () {
