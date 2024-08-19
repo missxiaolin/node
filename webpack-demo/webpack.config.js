79bc13909185a640b7dfcb5af7667a2d95d2af75
missxiaolin <462441355@qq.com>
2024-08-19 09:47:39 +0800

    webpack

---
+++ webpack-demo/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path')
+const Plugin = require('./src/plugin')
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        main: './src/index.js'
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: [
+                    // loader 的执行顺序从下到上
+                    {
+                        loader: path.resolve('./src/loader2.js'),
+                    },
+                    {
+                        loader: path.resolve('./src/loader1.js'),
+                    },
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new Plugin()
+    ]
+}
