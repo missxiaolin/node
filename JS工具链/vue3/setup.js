e8ed83201166be6d9ef04c16bc1162ba7d6a3b26
missxiaolin <462441355@qq.com>
2024-11-21 22:19:00 +0800

    * add

--- JS工具链/vue3/setup.js
+++ JS工具链/vue3/setup.js
@@ -38,41 +38,7 @@ traverse(ast, {
       node.init &&
       node.init.type === "ArrowFunctionExpression"
     ) {
-      //   console.log("node", node);
-      //   console.log("parent", parent);
       console.log(extractLines(b, node.loc.start.line, node.loc.end.line));
-      //   console.log("prop", prop);
-      //   console.log("idx", idx);
     }
-    // if (
-    //   node.type === "CallExpression" &&
-    //   node.callee.type === "MemberExpression"
-    // ) {
-    //   console.log("node.arguments[0]", node.arguments[0]);
-    //   console.log(JSON.stringify(node));
-    //   // return node;
-    // }
-    // if (
-    //   node.type === "CallExpression" &&
-    //   node.callee.type === "MemberExpression" &&
-    //   node.arguments[0] != undefined &&
-    //   node.arguments[0].type === "ObjectExpression"
-    // ) {
-    //   if (
-    //     node.callee.property.type === "Identifier" &&
-    //     config[node.callee.property.name] != undefined
-    //   ) {
-    //     let PROP_name = node.callee.property.name; //api的名称
-    //     let PROP = config[PROP_name]; //替换的参数对象
-    //     node.arguments[0].properties.forEach(function (element) {
-    //       if (PROP[element.key.name] != undefined) {
-    //         end = element.key.start;
-    //         let head = content.substring(begin, end); //element.key.start
-    //         begin = element.key.end;
-    //         contentArray.push(head, PROP[element.key.name]);
-    //       }
-    //     });
-    //   }
-    // }
   },
 });
