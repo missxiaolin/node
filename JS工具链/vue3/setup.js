dc127f063e81d361b621bee200dc35bc66efdfaa
missxiaolin <462441355@qq.com>
2024-11-22 11:29:39 +0800

    * 工具完成测试

--- JS工具链/vue3/setup.js
+++ JS工具链/vue3/setup.js
@@ -3,7 +3,14 @@ let parser = require("@babel/parser");
 
 const b = `export default {
     setup() {
-        const message = ref("Hello, Vue 3!");
+        let message = ref("Hello, Vue 3!");
+        let Obj = toRefs({
+            a: 1
+        });
+        let b = "bbbbbbbbb";
+        watch(Obj, (newValue, oldValue) => {
+            console.log(Obj)
+        });
         const ceshi = () => {
             console.log("ceshi1")
             console.log("ceshi2")
@@ -29,7 +36,9 @@ const ast = parser.parse(b, {
   sourceType: "module",
 });
 
-const config = {};
+let config = [];
+let vueExport = [];
+let str = "";
 
 traverse(ast, {
   pre: function (node, parent, prop, idx) {
@@ -38,7 +47,48 @@ traverse(ast, {
       node.init &&
       node.init.type === "ArrowFunctionExpression"
     ) {
-      console.log(extractLines(b, node.loc.start.line, node.loc.end.line));
+      // 方法
+      //   extractLines(b, node.loc.start.line, node.loc.end.line)
+      config.push(extractLines(b, node.loc.start.line, node.loc.end.line));
+    } else if (
+      // vue ref toRefs ……
+
+      node.type === "VariableDeclarator" &&
+      node.init &&
+      node.init.type === "CallExpression"
+    ) {
+      vueExport.push(node.init.callee.name);
+      str = `${str}${extractLines(
+        b,
+        node.loc.start.line,
+        node.loc.end.line
+      )}\n`;
+    } else if (
+      // 普通变量
+      node.type === "VariableDeclarator" &&
+      node.init &&
+      node.init.type === "StringLiteral"
+    ) {
+      str = `${str}${extractLines(
+        b,
+        node.loc.start.line,
+        node.loc.end.line
+      )}\n`;
+    } else if (
+      node.type === "ExpressionStatement" &&
+      node.expression &&
+      node.expression.type === "CallExpression" &&
+      node.expression.callee &&
+      node.expression.callee.name === "watch"
+    ) {
+      vueExport.push(node.expression.callee.name);
+      str = `${str}${extractLines(
+        b,
+        node.loc.start.line,
+        node.loc.end.line
+      )}\n`;
     }
   },
 });
+
+console.log(config, str, vueExport);
